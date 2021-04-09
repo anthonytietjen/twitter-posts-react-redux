a4bd45e779fc923ed92c019acf8856b51d073a85
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-09 01:58:48 -0600

    Add responsiveness

--- src/pages/SearchPage/SearchPageStyles.js
+++ src/pages/SearchPage/SearchPageStyles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
 export const StyledSearchPage = styled.div`
-    max-width: 600px;
-    padding: 15px;
+  .desktop {
+    max-width: 768px;
+    padding-top: 15px;
     margin: auto;
+  }
+
+  .desktop-view{
+    display: flex;
+    flex-direction: row;
+
+    > div{
+      flex-direction: column;
+      flex: 1;
+      margin-right: 15px;
+    }
+  }
+
+  .mobile-view{
+    display: flex;
+    flex-direction: column;
+  }
 `;

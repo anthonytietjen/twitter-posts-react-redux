e7e3516f3c08d5bcb82876bb96be4aa6d81b976f
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 14:26:52 -0600

    SearchResultRow styles

--- src/components/SearchResultRow/SearchResultRow.jsx
+++ src/components/SearchResultRow/SearchResultRow.jsx
@@ -1,9 +1,35 @@
+import { HashTag } from '../HashTag/HashTag';
 import { StyledSearchResultRow } from './SearchResultRowStyles';
 
 export const SearchResultRow = ({ tweet }) => (
   <StyledSearchResultRow
     data-testid="search_result_row"
   >
-    {tweet.text}
+    <div
+      className="profile-image"
+      style={{
+        backgroundColor: `#${tweet.user.profile_background_color}`,
+      }}
+    >
+      <img src={tweet.user.profile_image_url_https} alt="asdf" />
+    </div>
+    <div className="content-area">
+      <div className="screen-name-container">
+        <a
+          className="screen-name-link"
+          href={tweet.user.url}
+          target="_blank"
+        >
+          {'@'}
+          {tweet.user.screen_name}
+        </a>
+      </div>
+      <div className="text-container">
+        {tweet.text}
+      </div>
+      <div className="hashtags-container">
+        <HashTag key="1" text="hi" />
+      </div>
+    </div>
   </StyledSearchResultRow>
 );
--- src/components/SearchResultRow/SearchResultRowStyles.js
+++ src/components/SearchResultRow/SearchResultRowStyles.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 
 export const StyledSearchResultRow = styled.div`
-    border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+
+  .profile-image{
+    margin: 0;
+    border-radius: 50%;
+    width: 48px;
+    height: 48px;
+    overflow: hidden;
+    margin: 12px;
+
+    .img{
+      width: 48px;
+      height: 48px;
+    }
+  }
+  
+  .content-area{
+    flex: 1;
+    display: flex:
+    flex-direction: column;
+    margin: 12px;
+
+    .screen-name-container{
+      padding-bottom: 10px;
+
+      a.screen-name-link{
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+
+    .text-container{
+      padding-bottom: 10px;
+    }
+
+    .hashtags-container{
+      
+    }
+  }
 `;

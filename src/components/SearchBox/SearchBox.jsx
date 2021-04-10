b04a5442e9f02e159bff68f2fb2c7a2a4255a13a
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 11:27:07 -0600

    Add beginning of search functionality to SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { useEffect, useState } from 'react';
 import { StyledSearchBox } from './SearchBoxStyles';
+import { fetchTweets } from '../../api/api';
 
 const debounceTimeout = 1000; // milliseconds
 let debounceTimer;
@@ -7,20 +9,46 @@ let debounceTimer;
 export const SearchBox = () => {
   const [searchTerms, setSearchTerms] = useState('');
 
+  const { data, isFetching, refetch } = useQuery(
+    'fetchTweets',
+    () => fetchTweets(searchTerms),
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+    },
+  );
+
+  useEffect(() => {
+    if (data) {
+      // TODO dispatch the data
+      console.log({ data });
+    }
+  }, [data]);
+
   const handleChange = (e) => {
     clearTimeout(debounceTimer);
     const searchValue = e.target.value;
     setSearchTerms(searchValue);
 
     debounceTimer = setTimeout(() => {
-      // TODO: Perform search
+      refetch();
     }, debounceTimeout);
   };
+
+  console.log({ data, isFetching, refetch });
 
   return (
     <StyledSearchBox
       data-testid="search_box_view"
     >
+      <div className="search-icon-contaner">
+        {!isFetching && (
+          <span>S</span>
+        )}
+        {isFetching && (
+          <span>L</span>
+        )}
+      </div>
       <input
         data-testid="input_search_terms"
         value={searchTerms}
--- src/components/SearchBox/SearchBoxStyles.js
+++ src/components/SearchBox/SearchBoxStyles.js
@@ -4,8 +4,17 @@ export const StyledSearchBox = styled.div`
   display: block;
   margin-bottom: 10px;
 
+  .search-icon-contaner{
+    position:relative;
+
+    span{
+      position:absolute;
+      left: 15px;
+    }
+  }
+
   input {
-    padding: 8px;
+    padding: 8px 8px 8px 28px;
     border: 1px solid silver;
     display: block;
     border-radius: 0;

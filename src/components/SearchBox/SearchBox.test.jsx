3569de7973621dbb69f9beb265fe043f73336136
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 20:08:04 -0600

    Fix SearchBox test

--- src/components/SearchBox/SearchBox.test.jsx
+++ src/components/SearchBox/SearchBox.test.jsx
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SearchBox } from './SearchBox';
+import { store } from '../../store/store';
+
+const queryClient = new QueryClient();
 
 test('renders SearchBox compoenent', () => {
-  render(<SearchBox />);
+  render(
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SearchBox />
+      </QueryClientProvider>
+    </Provider>
+  );
+
   const inputSearchTerms = screen.getByTestId('input_search_terms');
   expect(inputSearchTerms).toBeInTheDocument();
 });

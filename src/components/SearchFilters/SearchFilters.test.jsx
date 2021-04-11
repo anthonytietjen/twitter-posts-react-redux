1dc3edd0aaea01d784d5bc408bae71793eedcfa0
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 20:23:09 -0600

    Fix SearchFilters test

--- src/components/SearchFilters/SearchFilters.test.jsx
+++ src/components/SearchFilters/SearchFilters.test.jsx
@@ -1,19 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SearchFilters } from './SearchFilters';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
 
 test('renders SearchFilters component', () => {
+  const queryClient = new QueryClient();
+  const store = mockStore({
+    search: {
+      tweets: [{
+        id: 1,
+        entities: {
+          hashtags: [
+            { text: "hashtag1" },
+            { text: "hashtag2" }
+          ]
+        },
+        text: 'text1'
+      }]
+    }
+  })
+
   render(
-    <SearchFilters
-      hashTags={["#hello", "#world"]}
-    />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SearchFilters />
+      </QueryClientProvider>
+    </Provider>
   );
 
   const textFilters = screen.getByTestId('text_filter_by_hashtag');
   expect(textFilters).toBeInTheDocument();
 
-  const hashTagHello = screen.getByTestId('text_#hello');
-  expect(hashTagHello).toBeInTheDocument();
+  const hashTag1 = screen.getByTestId('text_hashtag1');
+  expect(hashTag1).toBeInTheDocument();
 
-  const hashTagWorld = screen.getByTestId('text_#world');
-  expect(hashTagWorld).toBeInTheDocument();
+  const hashTag2 = screen.getByTestId('text_hashtag2');
+  expect(hashTag2).toBeInTheDocument();
 });

2fcae1a1806d283c68d06a4511a90f1989547736
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 20:59:55 -0600

    Fix SearchResults test

--- src/components/SearchResults/SearchResults.test.jsx
+++ src/components/SearchResults/SearchResults.test.jsx
@@ -1,8 +1,69 @@
 import { render, screen } from '@testing-library/react';
 import { SearchResults } from './SearchResults';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import configureStore from 'redux-mock-store';
 
-test('renders SearchResults compoenent', () => {
-  render(<SearchResults />);
+const mockStore = configureStore([]);
+
+test('renders SearchResults component', () => {
+  const queryClient = new QueryClient();
+
+  const tweet1 = {
+    id: 1,
+    entities: {
+      hashtags: [
+        { text: "hashtag1" },
+        { text: "hashtag2" }
+      ]
+    },
+    user: {
+      url: 'https://t.co/1',
+      screen_name: 'user1',
+      profile_background_color: 'EECCDD',
+      profile_image_url_https: 'https://t.co/1/image'
+    },
+    text: 'text1 https://t.co/1'
+  }
+  const tweet2 = {
+    id: 2,
+    entities: {
+      hashtags: [
+        { text: "hashtag3" },
+        { text: "hashtag4" }
+      ]
+    },
+    user: {
+      url: 'https://t.co/2',
+      screen_name: 'user2',
+      profile_background_color: 'EECCDD',
+      profile_image_url_https: 'https://t.co/2/image'
+    },
+    text: 'text1 https://t.co/2'
+  }
+
+  const store = mockStore({
+    search: {
+      searchTerms: 'hello',
+      tweets: [tweet1, tweet2],
+      nextResultId: 6
+    }
+  })
+
+  render(
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SearchResults />
+      </QueryClientProvider>
+    </Provider>
+  );
+
+  const resultRow1 = screen.getByTestId('search_result_row_1');
+  expect(resultRow1).toBeInTheDocument();
+
+  const resultRow2 = screen.getByTestId('search_result_row_2');
+  expect(resultRow2).toBeInTheDocument();
+
   const linkLoadMore = screen.getByTestId('link_load_more');
   expect(linkLoadMore).toBeInTheDocument();
 });

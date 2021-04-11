6a4e6e486acab6a991d3f82f977100b810f88b29
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 21:07:48 -0600

    Updates to SearchResultRow test

--- src/components/SearchResultRow/SearchResultRow.test.jsx
+++ src/components/SearchResultRow/SearchResultRow.test.jsx
@@ -5,59 +5,88 @@ import configureStore from 'redux-mock-store';
 
 const mockStore = configureStore([]);
 
-test('renders SearchResultRow compoenent', () => {
+describe('SearchResultRow', () => {
+  let tweet;
+  let store;
 
-  const tweet = {
-    id: 1,
-    entities: {
-      hashtags: [
-        { text: "hashtag1" },
-        { text: "hashtag2" }
-      ]
-    },
-    user: {
-      url: 'https://t.co/1',
-      screen_name: 'user1',
-      profile_background_color: 'EECCDD',
-      profile_image_url_https: 'https://t.co/1/image'
-    },
-    text: 'text1 https://t.co/1'
-  }
-
-  const store = mockStore({
-    search: {
-      tweets: [tweet]
+  beforeEach(() => {
+    tweet = {
+      id: 1,
+      entities: {
+        hashtags: [
+          { text: "hashtag1" },
+          { text: "hashtag2" }
+        ]
+      },
+      user: {
+        url: 'https://t.co/1',
+        screen_name: 'user1',
+        profile_background_color: 'EECCDD',
+        profile_image_url_https: 'https://t.co/1/image'
+      },
+      text: 'text1 https://t.co/1'
     }
+
+    store = mockStore({
+      search: {
+        tweets: [tweet]
+      }
+    })
   })
 
-  render(
-    <Provider store={store}>
-      <SearchResultRow tweet={tweet} />
-    </Provider>
-  );
+  it('renders even rows with a white background color', () => {
+    render(
+      <Provider store={store}>
+        <SearchResultRow tweet={tweet} />
+      </Provider>
+    );
 
-  const row = screen.getByTestId('search_result_row_1');
-  expect(row).toBeInTheDocument();
+    const row = screen.getByTestId('search_result_row_1');
+    expect(row).toHaveStyle('background-color: #FFFFFF');
+  });
 
-  const profileImage = screen.getByTestId('profile_image_1');
-  expect(profileImage).toBeInTheDocument();
-  expect(profileImage.src).toEqual('https://t.co/1/image');
+  it('renders odd rows with a gray background color', () => {
+    render(
+      <Provider store={store}>
+        <SearchResultRow tweet={tweet} isAlternateRow={true} />
+      </Provider>
+    );
 
-  const userScreenName = screen.getByTestId('screen_name_1');
-  expect(userScreenName).toBeInTheDocument();
-  expect(userScreenName.innerHTML).toEqual("@user1");
+    const row = screen.getByTestId('search_result_row_1');
+    expect(row).toHaveStyle('background-color: #F8F9F9');
+  });
 
-  const text = screen.getByTestId('text_1');
-  expect(text).toBeInTheDocument();
-  expect(text.innerHTML).toEqual("text1");
+  it('renders the avatar, screen name, text, link, and hashtags', () => {
+    render(
+      <Provider store={store}>
+        <SearchResultRow tweet={tweet} />
+      </Provider>
+    );
 
-  const tweetLink = screen.getByTestId('tweet_link_1');
-  expect(tweetLink).toBeInTheDocument();
-  expect(tweetLink.innerHTML).toEqual("https://t.co/1");
+    const row = screen.getByTestId('search_result_row_1');
+    expect(row).toBeInTheDocument();
 
-  const hashTag1 = screen.getByTestId('text_hashtag1');
-  expect(hashTag1).toBeInTheDocument();
+    const profileImage = screen.getByTestId('profile_image_1');
+    expect(profileImage).toBeInTheDocument();
+    expect(profileImage.src).toEqual('https://t.co/1/image');
 
-  const hashTag2 = screen.getByTestId('text_hashtag2');
-  expect(hashTag2).toBeInTheDocument();
+    const userScreenName = screen.getByTestId('screen_name_1');
+    expect(userScreenName).toBeInTheDocument();
+    expect(userScreenName.innerHTML).toEqual("@user1");
+
+    const text = screen.getByTestId('text_1');
+    expect(text).toBeInTheDocument();
+    expect(text.innerHTML).toEqual("text1");
+
+    const tweetLink = screen.getByTestId('tweet_link_1');
+    expect(tweetLink).toBeInTheDocument();
+    expect(tweetLink.innerHTML).toEqual("https://t.co/1");
+
+    const hashTag1 = screen.getByTestId('text_hashtag1');
+    expect(hashTag1).toBeInTheDocument();
+
+    const hashTag2 = screen.getByTestId('text_hashtag2');
+    expect(hashTag2).toBeInTheDocument();
+  });
+
 });

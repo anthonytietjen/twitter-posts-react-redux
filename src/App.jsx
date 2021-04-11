7b2c23e34259a3287c25f30f92d5bab56d806fce
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-04-10 19:59:23 -0600

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
 import { SearchPage } from './pages/SearchPage/SearchPage';
 
 function App() {
+  // If more pages get added in the future, add a Router here
   return (
     <SearchPage />
   );
